c6978eccc63cd323c68d41d74fc5e8042bbc1031
Clara\Clara Molina bwg <claramolinadev@gmail.com>
2026-01-31 14:23:03 +0100

    misc: aaa mas cosas

---
+++ My project/Assets/Scripts/RiVisualConfig.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "RiVisualConfig", menuName = "ScriptableObjects/Visuals/RiVisualConfig", order = 1)]
+public class RiVisualConfig : ScriptableObject
+{
+    [SerializeField] private List<Sprite> _riTris;
+    [SerializeField] private List<Sprite> _riChis;
+    [SerializeField] private List<Sprite> _riMos;
+    [SerializeField] private Sprite _dragonSed;
+    [SerializeField] private Sprite _oniSed;
+    [SerializeField] private Sprite _vacioSed;
+    
+    public Sprite GetRiSprite(Semilla semilla, int yu)
+    {
+        switch (semilla)
+        {
+            case Semilla.Chi:
+                return _riChis[yu];
+            case Semilla.Tri:
+                return _riTris[yu];
+            case Semilla.Mo:
+                return _riMos[yu];
+            default:
+                return null;
+        }
+    }
+
+}
