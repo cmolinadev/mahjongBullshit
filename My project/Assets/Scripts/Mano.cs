cd594bc32884354f0f296d4784733dd4840d8c99
Clara\Clara Molina bwg <claramolinadev@gmail.com>
2026-01-31 12:49:41 +0100

    feat: toggle handri stuff

--- My project/Assets/Scripts/Mano.cs
+++ My project/Assets/Scripts/Mano.cs
@@ -18,12 +18,18 @@ public class Mano : MonoBehaviour
 
     public void PedirRis()
     {
+        StartCoroutine(PedirRisRoutine());
+    }
+    public IEnumerator PedirRisRoutine()
+    {
        var data = _udaeta.GetRisData(_handRis.Count);
        foreach (var handRi in _handRis)
        {
+           yield return new WaitForSeconds(0.8f/2f);
            handRi.SetRiData(data[0]);
            data.RemoveAt(0);
            handRi.Show();
        }
     }
+    
 }
