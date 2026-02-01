5faaa18e1dc5a21a34af0f7a59defe2ca7b08a8a
Clara\Clara Molina bwg <claramolinadev@gmail.com>
2026-02-01 12:15:11 +0100

    feat: more audio

--- My project/Assets/Scripts/Dialogo.cs
+++ My project/Assets/Scripts/Dialogo.cs
@@ -10,14 +10,20 @@ public class Dialogo : MonoBehaviour
    [SerializeField] private writeText _writeText;
    [FormerlySerializedAs("_dialogues")] [SerializeField] private List<string> _vientoDialogues;
    [SerializeField] private List<string> _onFailureDialogues;
+   [SerializeField] private List<string> _refranesString;
 
    public void ShowRandomVientoText()
    {
+      FindFirstObjectByType<sfxManager>().Play(GetRandomRefranString());
       _writeText.changeText(_vientoDialogues[Random.Range(0, _vientoDialogues.Count)]);
    }
-
+   
    public void ShowRandomFailureText()
    {
+      FindFirstObjectByType<sfxManager>().Play(GetRandomRefranString());
       _writeText.changeText(_onFailureDialogues[Random.Range(0, _onFailureDialogues.Count)]);
    }
+   
+   private string GetRandomRefranString() => _refranesString[Random.Range(0, _refranesString.Count)];
+
 }
