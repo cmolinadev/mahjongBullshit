9c6a21c0e84983f211bca7d7bb27389b46d7f4e6
Clara\Clara Molina bwg <claramolinadev@gmail.com>
2026-02-01 02:45:02 +0100

    feat: chi counter

--- My project/Assets/Scripts/ChiBar.cs
+++ My project/Assets/Scripts/ChiBar.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChiBar : MonoBehaviour
 {
     [SerializeField] private Slider _chiSlider;
+    [SerializeField] TextMeshProUGUI _chiText;
     private float _maxChi;
 
     public void Initialize(int maxChi)
@@ -18,5 +20,6 @@ public class ChiBar : MonoBehaviour
     {
         Debug.Log("value = "+chi/_maxChi);
         _chiSlider.value = chi/_maxChi;
+        _chiText.text = chi.ToString();
     }
 }
